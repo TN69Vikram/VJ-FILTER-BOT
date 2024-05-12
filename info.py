8ba788db2d0933920906d4d7cc44d73ddb790687
TN69Vikram <169146640+TN69Vikram@users.noreply.github.com>
2024-05-12 09:12:00 +0530

    Update info.py

--- info.py
+++ info.py
@@ -127,11 +127,11 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "/")
+URL = environ.get("URL", "https://original420s.onrender.com/")
 
 
 # Rename Info ; If True Then Bot Rename File Else Not
-RENAME_MODE = bool(environ.get('RENAME_MODE', True)) # Set True or Flase
+RENAME_MODE = bool(environ.get('RENAME_MODE', Flase)) # Set True or Flase
 
 # Auto Approve Info ; If True Then Bot Approve New Upcoming Join Request Else Not
 AUTO_APPROVE_MODE = bool(environ.get('AUTO_APPROVE_MODE', True)) # Set True or Flase
